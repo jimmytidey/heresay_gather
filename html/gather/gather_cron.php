f87b269884f8fe48297a22355043dac918628a65
Jimmy Tidey <jimmytidey@gmail.com>
2013-04-02 09:35:36 +0100

    fixing paths for cron

--- html/gather/gather_cron.php
+++ html/gather/gather_cron.php
@@ -1,7 +1,7 @@
 <? 
 
 
-include (__DIR__ . '/../gather/header.php');
+include (__DIR__ . '/../header.php');
 
 //loads a class for reading RSS feeds
 include_once(__DIR__ . '/../classes/simplepie/autoloader.php');
@@ -14,7 +14,6 @@ foreach (glob(__DIR__ . "/../gather/sites/*.php") as $filename) {
 
 
 //Cycle through the values
-
 $current_value = $db->fetch('SELECT * FROM cron_manage');
 $current_value = $current_value[0]['current_value'];
 $sql    = "SELECT * FROM manual_sites WHERE site_id='$current_value'"; 
