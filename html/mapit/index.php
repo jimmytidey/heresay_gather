eb5e8a487921ec2da8d3f4c323899e56bdd2f031
Jimmy Tidey <jimmytidey@gmail.com>
2013-05-03 15:32:41 +0100

    adding mapit

---
+++ html/mapit/index.php
@@ -0,0 +1,78 @@
+<?
+
+include(__DIR__ . '/../ini.php');
+$db = new dbClass(DB_LOCATION, DB_USER_NAME, DB_PASSWORD, DB_NAME);
+
+$results = $db->fetch("SELECT * FROM manual_updates WHERE borough='' && LENGTH(postcode)>=5 LIMIT 3");
+
+foreach($results as $result) {
+    
+    sleep(1);
+    $lat = $result['lat'];
+    $lng = $result['lng'];
+    $id = $result['id'];
+        
+    $geo_url = 'http://mapit.mysociety.org/point/4326/'. $lng.','. $lat;
+
+    $ch = curl_init(); 
+
+    // set url 
+    curl_setopt($ch, CURLOPT_URL, $geo_url); 
+
+    //return the transfer as a string 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+    // $output contains the output string 
+    $output = curl_exec($ch); 
+
+    // close curl resource to free up system resources 
+    curl_close($ch);
+    
+    $location_data = json_decode($output, true);
+    
+   
+    
+    foreach($location_data as $location_datum) { 
+        if ($location_datum['type_name'] == 'London borough') { 
+            $borough = $location_datum['name'];
+            echo $borough;
+        }
+        
+        if ($location_datum['type_name'] == 'London borough ward') { 
+            $ward = $location_datum['name'];
+            echo $ward;
+        }
+        
+        if ($location_datum['type_name'] == 'UK Parliament constituency') { 
+            $constituency = $location_datum['name'];
+            echo $constituency;
+        }
+
+    }
+    
+
+    
+    $query  = "UPDATE manual_updates
+    SET borough='$borough', ward='$ward', constituency='$constituency'
+    WHERE id=$id";
+    $db->query($query);
+    echo $query;
+    
+
+    echo "<hr />";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
