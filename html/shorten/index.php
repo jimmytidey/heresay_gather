9ad43ceac07679f3cae90cf8a54c0d5a96cff3f0
Jimmy Tidey <jimmytidey@gmail.com>
2013-05-07 11:48:56 +0100

    automated commit

--- html/shorten/index.php
+++ html/shorten/index.php
@@ -2,7 +2,7 @@
 include(__DIR__ . '/../ini.php');
 $db = new dbClass(DB_LOCATION, DB_USER_NAME, DB_PASSWORD, DB_NAME);
 
-$results = $db->fetch("SELECT * FROM manual_updates WHERE short_url='' && lat != '--' LIMIT 50");
+$results = $db->fetch("SELECT * FROM manual_updates WHERE short_url='' LIMIT 100");
 
 foreach($results as $result) {
     
