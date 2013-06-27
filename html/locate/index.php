95d652906ba6389c34be0247aac8a4d839facc3a
Jimmy Tidey <jimmytidey@gmail.com>
2013-06-27 19:02:07 +0100

    automated commit

--- html/locate/index.php
+++ html/locate/index.php
@@ -16,7 +16,7 @@ if(!empty($lat) && !empty($lng)) {
     FROM manual_sites 
     JOIN manual_updates ON manual_sites.site = manual_updates.site 
     WHERE category_1='' 
-    ORDER BY distance ASC, pubdate DESC 
+    ORDER BY distance ASC 
     LIMIT 10";
     echo "<p>Locating by lat lng</p>";
 }
